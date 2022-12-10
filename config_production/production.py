fe60e86fde1bcb63a2126f9691f36e467e4e8e2a
ihong9059 <ihong9059@gmail.com>
2022-12-10 22:41:33 +0900

    using gunicorn

--- config_production/production.py
+++ config_production/production.py
@@ -1,11 +1,7 @@
-import os
-
-BASE_DIR = os.path.dirname(__file__)
+from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = b'Zb3\x81\xdb\xf1\xd9\xd7-Knb\x8eB\xa5\x18'
 
-SECRET_KEY = "dev"
 
-
-print('BASE_DIR:{}'.format(BASE_DIR))
