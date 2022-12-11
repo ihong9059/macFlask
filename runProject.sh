e0fbca268da9210ad7afcac899fb84f77ef8c222
root <root@ihongwindows.cafe24.com>
2022-12-11 11:52:58 +0900

    2022.12.11 make runTest at cafe24

--- runProject.sh
+++ runProject.sh
@@ -1,5 +1,5 @@
 export FLASK_APP=pybo
-export FLASK_ENV=development
+export FLASK_ENV=production
 export FLASK_DEBUG=true
-export APP_CONFIG_FILE=/Users/honggwangseon/uttec/macFlask/config/production.py 
+export APP_CONFIG_FILE=/root/uttec/macFlask/config/production.py 
 . ./myproject/bin/activate
