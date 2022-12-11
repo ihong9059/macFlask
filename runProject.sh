c1febdb9aebb5fb37595f2484c15d95e1647446a
ihong9059 <ihong9059@gmail.com>
2022-12-11 09:29:52 +0900

    test gunicorn

---
+++ runProject.sh
@@ -0,0 +1,5 @@
+export FLASK_APP=pybo
+export FLASK_ENV=development
+export FLASK_DEBUG=true
+export APP_CONFIG_FILE=/Users/honggwangseon/uttec/macFlask/config/production.py 
+. ./myproject/bin/activate
